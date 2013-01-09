e8405c447e0dc0a0873525cf1ed9c9f658ad4f16
Kelly Stannard <kwstannard@gmail.com>
2013-01-09 17:00:36 +0000

    first commit to test heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kwstannard::Application.config.secret_token = '9fa704102274567831a797128ee4c7bb1732771ca2dc76a813e735b902c49e570b9ac3f73c353cfc74d5519a0d9061f138a4fba598398144c29f46611dcf405c'
