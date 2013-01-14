d5c58672ea139da3e22e089520cb212a3137b08b
Kelly Stannard <kwstannard@gmail.com>
2013-01-14 14:15:47 +0000

    Redirecting root path to resume for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Kwstannard::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => 'resume#show'
   match '/resume' => 'resume#show'
 
   # Sample of regular route:
